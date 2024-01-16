b45a1ce761bef8a6aa52d5fdd5a63a77faad90e7
Pedro Marques <pdro.marqs2015@hotmail.com>
2024-01-16 01:12:31 -0300

    feat: GameImageBanner and simples principal home grid

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -1,7 +1,22 @@
+import Image from "next/image"
+
 export default function Home() {
   return (
-    <div>
-      Hello World
+    <div className="mt-6">
+      <div className="w-full flex justify-between">
+        {/* Banner do jogo */}
+        <div className="w-[72%] h-[550px]">
+          <Image
+            src="https://gamecraves.com/wp-content/uploads/2023/07/Stray-Header.jpg"
+            alt=""
+            width={1200}
+            height={500}
+            quality={100}
+            className="w-full h-full rounded-2xl"
+          />
+        </div>
+        <div className="border border-green-500 w-[25%] h-[550px]"></div>
+      </div>
     </div>
   )
 }
