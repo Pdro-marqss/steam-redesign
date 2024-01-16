a1fbb09c2e328ab30173da43a9f82870eef9fc79
Pedro Marques <pdro.marqs2015@hotmail.com>
2024-01-16 01:19:12 -0300

    feat: responsive banner with aspect-ratio

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -5,17 +5,17 @@ export default function Home() {
     <div className="mt-6">
       <div className="w-full flex justify-between">
         {/* Banner do jogo */}
-        <div className="w-[72%] h-[550px]">
+        <div className="w-[72%]">
           <Image
             src="https://gamecraves.com/wp-content/uploads/2023/07/Stray-Header.jpg"
             alt=""
             width={1200}
             height={500}
             quality={100}
-            className="w-full h-full rounded-2xl"
+            className="max-w-full rounded-2xl aspect-video"
           />
         </div>
-        <div className="border border-green-500 w-[25%] h-[550px]"></div>
+        <div className="border border-green-500 w-[25%]"></div>
       </div>
     </div>
   )
